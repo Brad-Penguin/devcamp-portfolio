92b6dd988623ace3b14c280802c5968d394ae710
Brad <bverd08@gmail.com>
2021-08-15 12:41:29 -0400

    created seeds files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+    Blog.create!(
+        title: "My blog  #{blog}",
+        body: "yeeet"
+    )
+end
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end 
+
+puts '5 skills made'
+
+9.times do |portfolio_item|
+    Portfolio.create!(
+        title: "port item #{portfolio_item}",
+        subtitle: "yeet 2",
+        body: "OKOK",
+        main_image: 'http://placehold.it/600x400' ,
+        thumb_image: 'http://placehold.it/350x200' 
+    )
+end
